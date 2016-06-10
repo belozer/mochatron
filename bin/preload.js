4ab2e6d45aefba190fe87f0b9861613656f22865
Konstantin Gladkikh <birhoff@users.noreply.github.com>
2016-06-10 15:27:06 +0300

    Update preload.js

--- bin/preload.js
+++ bin/preload.js
@@ -1,13 +1,24 @@
 'use strict';
 
+const path = require('path');
 var electron = require('electron');
 // Use sprintf because the browser console.log does it, but node does not.
 var sprintf = require('sprintf-js').sprintf;
 
+let reporter = undefined;
+if(process.env.MOCHA_PHANTOM_REPORTER){
+    reporter = require(path.resolve(process.env.MOCHA_PHANTOM_REPORTER));
+}
+
 var mochatron = {
     run: function(cb) {
         var mocha = window.mocha;
-        mocha.setup({ reporter: 'spec' });
+        mocha.setup({ reporter: reporter || 'spec' });
+        // Mocha needs a process.stdout.write in order to change the cursor position.
+        Mocha.process = Mocha.process || {};
+        Mocha.process.stdout = Mocha.process.stdout || process.stdout;
+        Mocha.process.stdout.write = function(s) { sendMsg('mocha', 'stdout', s) };
+
         mocha.run(function(failCount) {
             cb();
             sendMsg('mocha', 'testRunEnded', failCount);
