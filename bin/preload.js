73e3f7fc02b00c8bfbfc8c2e4da043a66ec393d6
Konstantin Gladkikh <birhoff@users.noreply.github.com>
2016-06-10 14:01:05 +0300

    Update preload.js

--- bin/preload.js
+++ bin/preload.js
@@ -5,70 +5,74 @@ var electron = require('electron');
 var sprintf = require('sprintf-js').sprintf;
 
 var mochatron = {
-  run: function() {
-    var mocha = window.mocha;
-    mocha.setup({
-      reporter: 'spec'
-    })
-    mocha.run(doneCallback);
-  }
-}
+    run: function(cb) {
+        var mocha = window.mocha;
+        mocha.setup({ reporter: 'spec' });
+        mocha.run(function(failCount) {
+            cb();
+            sendMsg('mocha', 'testRunEnded', failCount);
+        });
+    }
+};
 
 if (window) {
-  window.mochatron = mochatron;
+    window.mochatron = mochatron;
+
+    window.onerror = function(message, filename, lineno, colno, err) {
+        sendMsg('mocha', 'error', {
+            message: message,
+            filename: filename,
+            err: err,
+            stack: err.stack
+        });
+    };
 }
 
-function sendMsg(type, message) {
-  electron.ipcRenderer.send(type, message);
-}
-
-function doneCallback(errorCount) {
-  if (errorCount > 0) {
-    sendMsg('error', errorCount);
-  }
+function sendMsg(channel, type, message) {
+    electron.ipcRenderer.send(channel, type, message);
 }
 
 // Taken from nightmare-js
-(function(){
-  // listen for console.log
-  var defaultLog = console.log;
-  console.log = function() {
-    var message = sprintf.apply(this, [].slice.call(arguments));
-    // This 'if' is kind hacky, not sure why 'stdout:' is being output to the console, maybe it's ANSI related?
-    if (message !== 'stdout:') {
-      electron.ipcRenderer.send('console', 'log', message);
-      return defaultLog.apply(this, arguments);
-    }
-  };
+(function() {
+    // listen for console.log
+    var defaultLog = console.log;
+    console.log = function() {
+        var message = sprintf.apply(this, [].slice.call(arguments));
+        // This 'if' is kind hacky, not sure why 'stdout:' is being output to the console, maybe it's ANSI related?
+        if (message !== 'stdout:') {
+            electron.ipcRenderer.send('console', 'log', message);
+            return defaultLog.apply(this, arguments);
+        }
+    };
 
-  // listen for console.warn
-  var defaultWarn = console.warn;
-  console.warn = function() {
-    var message = sprintf.apply(this, [].slice.call(arguments));
-    electron.ipcRenderer.send('console', 'warn', message);
-    return defaultWarn.apply(this, arguments);
-  };
+    // listen for console.warn
+    var defaultWarn = console.warn;
+    console.warn = function() {
+        var message = sprintf.apply(this, [].slice.call(arguments));
+        electron.ipcRenderer.send('console', 'warn', message);
+        return defaultWarn.apply(this, arguments);
+    };
 
-  // listen for console.error
-  var defaultError = console.error;
-  console.error = function() {
-    var message = sprintf.apply(this, [].slice.call(arguments));
-    electron.ipcRenderer.send('console', 'error', message);
-    return defaultError.apply(this, arguments);
-  };
+    // listen for console.error
+    var defaultError = console.error;
+    console.error = function() {
+        var message = sprintf.apply(this, [].slice.call(arguments));
+        electron.ipcRenderer.send('console', 'error', message);
+        return defaultError.apply(this, arguments);
+    };
 
-  // overwrite the default alert
-  window.alert = function(message){
-    electron.ipcRenderer.send('page', 'alert', message);
-  };
+    // overwrite the default alert
+    window.alert = function(message) {
+        electron.ipcRenderer.send('page', 'alert', message);
+    };
 
-  // overwrite the default prompt
-  window.prompt = function(message, defaultResponse){
-    electron.ipcRenderer.send('page', 'prompt', message, defaultResponse);
-  }
+    // overwrite the default prompt
+    window.prompt = function(message, defaultResponse) {
+        electron.ipcRenderer.send('page', 'prompt', message, defaultResponse);
+    };
 
-  // overwrite the default confirm
-  window.confirm = function(message, defaultResponse){
-    electron.ipcRenderer.send('page', 'confirm', message, defaultResponse);
-  }
-})()
+    // overwrite the default confirm
+    window.confirm = function(message, defaultResponse) {
+        electron.ipcRenderer.send('page', 'confirm', message, defaultResponse);
+    };
+})();
